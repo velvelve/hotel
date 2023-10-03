f411d24743c7b8c26804a1e9a87fe8645f3fc193
root <root@DESKTOP-M27IL6F>
2023-10-03 19:23:25 +0300

    добавил календарь на главный экран

--- resources/views/home/welcome.blade.php
+++ resources/views/home/welcome.blade.php
@@ -1,2 +1,24 @@
+@extends('layouts.main')
 
-<h1>Hello</h1>
+@section('content')
+
+
+<form action="" method="POST">
+    @csrf
+    <input type="text" name="date_range" id="date_range" required>
+
+    <label for="guest_count">Количество гостей:</label>
+    <input type="number" name="guest_count" id="guest_count" required>
+    <button type="submit">Искать</button>
+
+</form>
+<script>
+    $(function() {
+        $('input[name="date_range"]').daterangepicker({
+            autoApply: true,
+            opens: 'left',
+            minDate: new Date(),
+        })
+    });
+</script>
+@endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,21 +8,19 @@
 
     <style>
     </style>
-
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
 </head>
 
 <body>
-
     <main>
-
-
         <div>
             @yield('content')
         </div>
-
-
     </main>
-
 </body>
+
 
 </html>

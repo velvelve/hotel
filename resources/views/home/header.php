ed38a35500df69b925a1fa96d0856d01a0d1d81a
Andrei P <andrei_pavlishin@bk.ru>
2023-10-04 03:18:36 +0300

    Сверстана header адапртивный Стили для header JS для бургер меню

---
+++ resources/views/home/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ resources('css/header.css') }}">
+    <title>Гостиница</title>
+</head>
+
+<body>
+    <header class="header">
+        <div class="header_container container">
+            <div class="menu">
+                <a href="#">
+                    <img src="./img/logo.svg" alt="logo">
+                </a>
+                <nav>
+                    <ul class="menu_ul">
+                        <li><a class="menu_li" href="#">Главная</a></li>
+                        <li><a class="menu_li" href="#">Номера</a></li>
+                        <li><a class="menu_li" href="#">Контакты</a></li>
+                        <li><a class="menu_li" href="#">Войти</a></li>
+                        <li><a class="menu_li" href="#">Регистрация</a></li>
+                    </ul>
+                    <div class="header_content">
+                        <div class="menu-btn">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                </nav>
+            </div>
+        </div>
+
+        </div>
+    </header>
+    <script type="text/javascript" src="{{ resources('js/header.js') }}"></script>
+</body>
+
+</html>

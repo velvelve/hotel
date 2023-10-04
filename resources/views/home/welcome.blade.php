9597bbdc25cf566a6b520084c86f13db741cedfc
root <root@DESKTOP-M27IL6F>
2023-10-04 19:18:45 +0300

    поправил форматирование

--- resources/views/home/welcome.blade.php
+++ resources/views/home/welcome.blade.php
@@ -1,24 +1,22 @@
 @extends('layouts.main')
 
 @section('content')
+    <form action="{{ route('search.rooms') }}" method="POST">
+        @csrf
+        <input type="text" name="date_range" id="date_range" required>
+        <label for="guest_count">Количество гостей:</label>
+        <input type="number" name="guest_count" id="guest_count" required>
+        <button type="submit">Искать</button>
+        <br>
+    </form>
 
-
-<form action="{{ route('search.rooms') }}" method="POST">
-    @csrf
-    <input type="text" name="date_range" id="date_range" required>
-    <label for="guest_count">Количество гостей:</label>
-    <input type="number" name="guest_count" id="guest_count" required>
-    <button type="submit">Искать</button>
-    <br>
-
-</form>
-<script>
-    $(function() {
-        $('input[name="date_range"]').daterangepicker({
-            autoApply: true,
-            opens: 'left',
-            minDate: new Date(),
-        })
-    });
-</script>
+    <script>
+        $(function() {
+            $('input[name="date_range"]').daterangepicker({
+                autoApply: true,
+                opens: 'left',
+                minDate: new Date(),
+            })
+        });
+    </script>
 @endsection

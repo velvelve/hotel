d9f6a4eea0cb5aff303f6e073f7b59142ca8f000
[Artyomka] <[miura.aruchomu@gmail.com]>
2023-10-04 11:06:56 +0300

    H-10 Вынес верстку поиска номеров в отдельный компонент

--- resources/views/home/welcome.blade.php
+++ resources/views/home/welcome.blade.php
@@ -1,33 +1,17 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="roomsSearch">
-    <div class="roomsSearch-head">
-        Бронирование
-    </div>
-    <div class="roomsSearch-menu">
-        <form action="" method="POST">
-            @csrf
-            <div class="roomsSearch-menu__date">
-                <div class="roomsSearch-menu__date-head">
-                    <img src="/img/search/date.svg">
-                    <label for="date_range">Заезд - Выезд</label>
-                </div>
-                <input type="text" name="date_range" id="date_range" required>
-            </div>
-            <div class="roomsSearch-menu__guest">
-                <div class="roomsSearch-menu__guest-head">
-                    <img src="/img/search/guest.svg">
-                    <label for="guest_count">Кол-во гостей</label>
-                </div>
-                <input type="number" name="guest_count" id="guest_count" required>
-            </div>
-            <button type="submit" class="roomsSearch-menu__btn">Искать</button>
 
-        </form>
-    </div>
-</div>
 
+<form action="{{ route('search.rooms') }}" method="POST">
+    @csrf
+    <input type="text" name="date_range" id="date_range" required>
+    <label for="guest_count">Количество гостей:</label>
+    <input type="number" name="guest_count" id="guest_count" required>
+    <button type="submit">Искать</button>
+    <br>
+
+</form>
 <script>
     $(function() {
         $('input[name="date_range"]').daterangepicker({

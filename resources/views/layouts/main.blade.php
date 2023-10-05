2913ad5465702094777c2e990ca64176bc5002ff
MiuraAruchomu <76885969+MiuraAruchomu@users.noreply.github.com>
2023-10-05 18:58:39 +0300

    Удалил DS_Store

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,7 +15,11 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Forum&family=Manrope:wght@300;500;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Forum&family=Montserrat:wght@300;400&display=swap" rel="stylesheet">
     <link href="{{asset('css/roomsSearch/roomsSearch.css')}}" rel="stylesheet">
+    @stack('styles')
 </head>
 
 <body>

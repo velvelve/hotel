dc7e7c56150e480d97d231c65832fca5711d8c46
Andrei P <andrei_pavlishin@bk.ru>
2023-10-07 22:27:07 +0300

    Полностью доработал адаптив

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,26 +1,24 @@
 <div class="wrapper">
     <header class="header">
-        <div class="header_container container">
-            <a href="#">
-                <img src="{{asset('img/von.png')}}" alt="logo">
-            </a>
-            <div class="header_content">
-                <div class="menu-btn">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
-                <div class="menu">
-                    <nav>
-                        <ul class="menu_ul">
-                            <li><a class="menu_li" href="#">Главная</a></li>
-                            <li><a class="menu_li" href="#">Номера</a></li>
-                            <li><a class="menu_li" href="#">Контакты</a></li>
-                            <li><a class="menu_li" href="#">Войти</a></li>
-                            <li><a class="menu_li" href="#">Регистрация</a></li>
-                        </ul>
-                    </nav>
-                </div>
+        <a href="#">
+            <img src="{{asset('img/von.png')}}" alt="logo">
+        </a>
+        <div class="header_content">
+            <div class="menu-btn">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div class="menu">
+                <nav>
+                    <ul class="menu_ul">
+                        <li><a class="menu_li" href="#">Главная</a></li>
+                        <li><a class="menu_li" href="#">Номера</a></li>
+                        <li><a class="menu_li" href="#">Контакты</a></li>
+                        <li><a class="menu_li" href="#">Войти</a></li>
+                        <li><a class="menu_li" href="#">Регистрация</a></li>
+                    </ul>
+                </nav>
             </div>
         </div>
     </header>

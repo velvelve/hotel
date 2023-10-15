fa258e4af4ef6fd5eb9703a9b0d3107ac5a0ae37
root <root@DESKTOP-M27IL6F>
2023-10-15 21:05:31 +0300

    поправил файл слайдер, объеденил welcome

--- resources/views/components/slider/slider.blade.php
+++ resources/views/components/slider/slider.blade.php
@@ -4,55 +4,32 @@
     </a>
     <!-- Загрузка номеров -->
     <x-sliderCart.sliderCart />
-    <div class="slider-cart">
-        <!-- Загрузка изображения -->
-        <img src="/img/homePage/section2/cart-img.png" class="cart-img" />
-        <div class="slider-cart-description">
-            <div class="description-wrapper">
-                <!-- Загрузка уровня номера -->
-                <div class="description-level">Номер SUPERIOR</div>
-                <!-- Загрузка верхнего описания -->
-                <div class="description-upperText">23 m2 | 2 кровати TWIN или 1 кровать Queen
-                    | 2 взрослых, 1 ребенок (0-11 лет)</div>
-                <!-- Загрузка иконок -->
-                <div class="description-icons">
-                    <img src="/img/homePage/section2/slippers.svg" class="description-ico">
-                    <img src="/img/homePage/section2/kettle.svg" class="description-ico">
-                    <img src="/img/homePage/section2/wifi.svg" class="description-ico">
-                    <img src="/img/homePage/section2/tv.svg" class="description-ico">
-                    <img src="/img/homePage/section2/hair-dryer.svg" class="description-ico">
+    @foreach ($rooms as $room)
+        <div class="slider-cart">
+            <img src="{{ $room->images[0]->path }}" class="cart-img" />
+            <div class="slider-cart-description">
+                <div class="description-wrapper">
+                    <!-- Загрузка уровня номера -->
+                    <div class="description-level">Номер {{ $room->room_type }}</div>
+                    <!-- Загрузка верхнего описания -->
+                    <div class="description-upperText">18 m2 | 1 кровать Queen | 2 взрослых, 1 ребенок (0-11
+                        лет)
+                    </div>
+                    <!-- Загрузка иконок -->
+                    <div class="description-icons">
+                        @foreach ($room->includedServices as $service)
+                            <div class="description-icon-container">
+                                <img src="{{ $service->icon()->path }}" class="description-ico">
+                            </div>
+                        @endforeach
+                    </div>
+                    <!-- Загрузка нижнего описания -->
+                    <div class="description-lowerText">В номерах Standart есть все необходимое для комфортного
+                        пребывания и спокойного сна.</div>
                 </div>
-                <!-- Загрузка нижнего описания -->
-                <div class="description-lowerText">Если вам необходим дополнительный комфорт, забронируйте номер
-                    Superior. В этом номере есть все, что вам нужно.</div>
             </div>
         </div>
-    </div>
-    <div class="slider-cart">
-        <!-- Загрузка изображения -->
-        <img src="/img/homePage/section2/cart-img.png" class="cart-img" />
-        <div class="slider-cart-description">
-            <div class="description-wrapper">
-                <!-- Загрузка уровня номера -->
-                <div class="description-level">Номер PREMIUM</div>
-                <!-- Загрузка верхнего описания -->
-                <div class="description-upperText">30 m2 | 2 кровати TWIN или 1 кровать Queen
-                    | 2 взрослых, 1 ребенок (0-11 лет)</div>
-                <!-- Загрузка иконок -->
-                <div class="description-icons">
-                    <img src="/img/homePage/section2/slippers.svg" class="description-ico">
-                    <img src="/img/homePage/section2/kettle.svg" class="description-ico">
-                    <img src="/img/homePage/section2/wifi.svg" class="description-ico">
-                    <img src="/img/homePage/section2/tv.svg" class="description-ico">
-                    <img src="/img/homePage/section2/hair-dryer.svg" class="description-ico">
-                    <img src="/img/homePage/section2/safe.svg" class="description-ico">
-                </div>
-                <!-- Загрузка нижнего описания -->
-                <div class="description-lowerText">Наши номера Premium подойдут тем, кто любит просторные
-                    помещения и дополнительный комфорт.</div>
-            </div>
-        </div>
-    </div>
+    @endforeach
     <a class="slider-btn">
         <img src="/img/homePage/section2/polygon-right.svg" class="slider-btn-img">
     </a>

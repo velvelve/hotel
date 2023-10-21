2b55475561764e9e9380b0244343805c8612ea10
Xenon <SDA152nn@gmail.com>
2023-10-21 05:37:36 +0300

    Корректировка footer
    -Добавлены иконки
    -Дописан css
    -Добавлены эффекты наведения

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -6,42 +6,40 @@
         <div class="footer_top">
 
             <div class="footer_logo">
-                <img class="footer_img" src="{{ asset('img/von.png') }}" alt="logo">
+                <img class="footer_img" src="{{ asset('img\footer\logo.png') }}" alt="logo">
                 <p class="copyright">© {{ date('Y') }} LUXURY HOTEL</p>
             </div>
 
-            <div class="footer_contacts">
-
                 <div>
                     <ul class="footer_list">
-                        <li><a href="#">Об отеле</a></li>
-                        <li><a href="#">Номера и цены</a></li>
-                        <li><a href="#">Рестораны</a></li>
-                        <li><a href="#">Спа</a></li>
-                        <li><a href="#">Правовая информация</a></li>
+                        <li><a class="footer__link" href="#">Об отеле</a></li>
+                        <li><a class="footer__link" href="#">Номера и цены</a></li>
+                        <li><a class="footer__link" href="#">Рестораны</a></li>
+                        <li><a class="footer__link" href="#">Спа</a></li>
+                        <li><a class="footer__link" href="#">Правовая информация</a></li>
                     </ul>
                 </div>
 
                 <div>
                     <ul class="footer_list">
-                        <li><a href="#">Конференц-залы</a></li>
-                        <li><a href="#">Контакты</a></li>
-                        <li><a href="#">Бронирование</a></li>
-                        <li><a href="#">Аренда</a></li>
-                        <li><a href="#">Услуги</a></li>
+                        <li><a class="footer__link" href="#">Конференц-залы</a></li>
+                        <li><a class="footer__link" href="#">Контакты</a></li>
+                        <li><a class="footer__link" href="#">Бронирование</a></li>
+                        <li><a class="footer__link" href="#">Аренда</a></li>
+                        <li><a class="footer__link" href="#">Услуги</a></li>
                     </ul>
                 </div>
 
-                <div>
+                <div class="footer__contacts">
                     <ul class="footer_list contacts">
                         <li class="adress">
                             <p>г. Москва, ул. Никольская, 123</p>
                         </li>
                         <li>
-                            <a href="tel:+78008008880">+7-800-800-88-80</a>
+                            <a class="footer__link" href="tel:+78008008880">+7-800-800-88-80</a>
                         </li>
                         <li>
-                            <a href="mailto:info@luxuryhotel.ru">info@luxury_hotel.ru</a>
+                            <a class="footer__link" href="mailto:info@luxuryhotel.ru">info@luxury_hotel.ru</a>
                         </li>
                     </ul>
                 </div>
@@ -50,19 +48,17 @@
                     <p class="social_p">Связаться с нами</p>
 
                     <div class="social_link">
-                        <a href="#"><img src="{{ asset('images/youtube.png') }}" alt="youtube"></a>
-                        <a href="#"><img src="{{ asset('images/vk.png') }}" alt="vk"></a>
-                        <a href="#"><img src="{{ asset('images/facebook.png') }}" alt="facebook"></a>
-                        <a href="#"><img src="{{ asset('images/twitter.png') }}" alt="twitter"></a>
-                        <a href="#"><img src="{{ asset('images/tiktok.png') }}" alt="tiktok"></a>
-                        <a href="#"><img src="{{ asset('images/instagram.png') }}" alt="instagram"></a>
-                        <a href="#"><img src="{{ asset('images/telegram.png') }}" alt="telegram"></a>
-                        <a href="#"><img src="{{ asset('images/whatsapp.png') }}" alt="whatsapp"></a>
+                        <a href="#"><img class="social__link-icon" src="{{ asset('img\footer\youtube.png') }}" alt="youtube"></a>
+                        <a href="#"><img class="social__link-icon" src="{{ asset('img\footer\vk.png') }}" alt="vk"></a>
+                        <a href="#"><img class="social__link-icon" src="{{ asset('img\footer\facebook.png') }}" alt="facebook"></a>
+                        <a href="#"><img class="social__link-icon" src="{{ asset('img\footer\twitter.png') }}" alt="twitter"></a>
+                        <a href="#"><img class="social__link-icon" src="{{ asset('img\footer\tiktok.png') }}" alt="tiktok"></a>
+                        <a href="#"><img class="social__link-icon" src="{{ asset('img\footer\instagram.png') }}" alt="instagram"></a>
+                        <a href="#"><img class="social__link-icon" src="{{ asset('img\footer\telegram.png') }}" alt="telegram"></a>
+                        <a href="#"><img class="social__link-icon" src="{{ asset('img\footer\whatsapp.png') }}" alt="whatsapp"></a>
                     </div>
 
                 </div>
-
-            </div>
 
         </div>
 
